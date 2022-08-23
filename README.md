0d5ec93e2c71cea8529a936b809418abf6716baa
ladyinblack <ladyinblack2010rs@gmail.com>
2022-08-23 08:01:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-rfteh1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-rfteh1)
---
+++ index.js
@@ -0,0 +1,64 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>Use Inheritance So You Don't Repeat Yourself</h1>`;
+
+/** TODO:
+ * The eat method is repeated in both Cat and Bear.  Edit the code in the spirit of DRY by moving the eat method to the Animal supertype.
+ * 
+ function Cat(name) {
+   this.name = name;
+ }
+ 
+ Cat.prototype = {
+   constructor: Cat,
+   eat: function() {
+     console.log("nom nom nom");
+   }
+ };
+ 
+ function Bear(name) {
+   this.name = name;
+ }
+ 
+ Bear.prototype = {
+   constructor: Bear,
+   eat: function() {
+     console.log("nom nom nom");
+   }
+ };
+ 
+ function Animal() {}
+ 
+ Animal.prototype = {
+   constructor: Animal,
+ 
+ }
+ */
+
+function Cat(name) {
+  this.name = name;
+}
+
+Cat.prototype = {
+  constructor: Cat,
+};
+
+function Bear(name) {
+  this.name = name;
+}
+
+Bear.prototype = {
+  constructor: Bear,
+};
+
+function Animal() {}
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function () {
+    console.log('nom nom nom');
+  },
+};
